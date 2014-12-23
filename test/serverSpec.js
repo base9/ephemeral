40269bc4d072eb2a2080fd2081817676a1992618
espiler <monkeysrawesome@gmail.com>
2014-12-23 15:56:51 -0800

    sets up basic testing & integration

---
+++ test/serverSpec.js
@@ -0,0 +1,41 @@
+var should = require('should');
+var expect = require("chai").expect;
+var request = require('request');
+ 
+describe("Server", function(){
+
+  describe("Endpoints", function(){
+    it("should have a responsive GET api/events/  ", function(done) {
+ 			request.get('http://127.0.0.1:9000/api/events', function(err, res, body) {
+ 				expect(res.statusCode).to.equal(200);
+ 				done();
+ 			})
+    });
+
+    it("should have a responsive GET api/events/:id  ", function(done) {
+    	// Checks events id:1, will not work if database is completely empty or if event id: 1 has been cleared
+ 			request.get('http://127.0.0.1:9000/api/events/1', function(err, res, body) {
+ 				expect(res.statusCode).to.equal(200);
+ 				done();
+ 			})
+    });
+
+    it("should have a responsive POST api/events/  ", function(done) {
+    	// Checks events id:1, will not work if database is completely empty or if event id: 1 has been cleared
+ 			request
+ 				.post('http://127.0.0.1:9000/api/events/')
+ 				.form({
+ 					lat: 37.884541,  
+ 					lng: -122.304272,
+ 					title: "Evan's Awesome Party!",
+ 					info: "Is Rockin' like 1995"
+ 				})
+ 				.on('response', function (res) {
+ 				  expect(res.statusCode).to.equal(201);
+ 				  done();
+ 				});
+    });
+
+  });
+
+});
