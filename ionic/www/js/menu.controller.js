a99fb880e5209c38e4d3129116790eb5ef26142a
espiler <monkeysrawesome@gmail.com>
2014-12-30 19:18:08 -0800

    adds search in navbar

--- ionic/www/js/menu.controller.js
+++ ionic/www/js/menu.controller.js
@@ -1,6 +1,25 @@
 angular.module('starter')
-.controller('MenuController', function($scope, $ionicSideMenuDelegate,$ionicNavBarDelegate) {
+.controller('MenuController', function($scope, $ionicSideMenuDelegate, $ionicNavBarDelegate, $timeout) {
 	$scope.toggleRight = function() {
-	   $ionicSideMenuDelegate.toggleRight();
-	 };
+	  $ionicSideMenuDelegate.toggleRight();
+	};
+
+	$scope.showSearch = false;
+
+// This is an ugly hack -- Figure out real angular/ionic ready function
+	$timeout(function() {
+		$ionicNavBarDelegate.handle = 'navBar';
+		$ionicNavBarDelegate.title('Playdar!');
+	}, 150);
+
+	$scope.test = function() {
+		console.log("seearchtext")
+	}
+
+
+	$scope.newSearch = function() {
+		$scope.search = ''
+		$scope.showSearch = !$scope.showSearch;
+	}
+
 })
