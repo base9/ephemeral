b2aa998afd3ed3b0b57695c6a2bfab13dd8638bd
Brian Scoles <brianscoles@gmail.com>
2014-12-30 17:22:00 -0800

    invert truth value on dummy checks

--- server/api/ratings/ratings.seed.js
+++ server/api/ratings/ratings.seed.js
@@ -6,7 +6,7 @@ setTimeout(function(){
   Event.where({title:'Free Bagels at 8pm'}).fetch().then(function (evnt) {
     if(evnt){
       Rating.where({comment:'The bagels were like, soooo stale'}).fetch().then(function (rating) {
-        if(rating){
+        if(!rating){
           var newRating = new Rating({
             comment: 'The bagels were like, soooo stale',
             user_id: 1,
@@ -23,7 +23,7 @@ setTimeout(function(){
   Event.where({title:'SantaCon'}).fetch().then(function (evnt) {
     if(evnt){
       Rating.where({comment:'Santa was very friendly'}).fetch().then(function (rating) {
-        if(rating){
+        if(!rating){
           var newRating1 = new Rating({
             comment: 'Santa was very friendly',
             user_id: 1,
