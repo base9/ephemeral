375c8c723332fcc8b5796401794f8100fe5e4534
espiler <monkeysrawesome@gmail.com>
2014-12-31 12:20:13 -0800

    refactors marker placement, adds info window css

--- ionic/www/js/map.controller.js
+++ ionic/www/js/map.controller.js
@@ -1,5 +1,11 @@
 angular.module('radar')
-.controller('MapController', ['MapFactory', 'MapSearchFactory', 'MarkerFactory', 'HttpHandler', '$scope', function(Map, SearchBox, Marker, Http, $scope) {
+.controller('MapController', [
+	'MapFactory', 
+	'MapSearchFactory', 
+	'MarkerFactory', 
+	'HttpHandler', 
+	'$scope', 
+	function(Map, SearchBox, Marker, Http, $scope) {
   
   $scope.geoLocate = function(){
     console.log("LOCATING.......")
@@ -9,19 +15,9 @@ angular.module('radar')
   var map = Map.initialize();
   var myMarker = Map.geoLocate();
 
-  // SearchBox.initialize(map);
 
-  // var goToPlace = function () {
-  //   var places = SearchBox.searchBox.getPlaces();
-  //   if (places.length) {
-  //     Map.goToPlace(places[0])
-  //   }
-  // }
-
-  // google.maps.event.addListener(SearchBox.searchBox, 'places_changed', goToPlace);
-
-  Http.getMarkers(function(title, places) {
-    Marker.placeMarkers(map, title, places, Http);
+  Http.getMarkers(function(events) {
+    Marker.placeMarkers(map, events, Http);
   });
 
-}]);
+}])
