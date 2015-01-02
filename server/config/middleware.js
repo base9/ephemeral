13b267c6a22ea94c21a025bb8d17462a2a4f9670
Stephan Genyk <sgenyk91@gmail.com>
2015-01-02 12:07:24 -0800

    Adds passport.js, moves middleware.js and passport.js to config folder, creates test button

---
+++ server/config/middleware.js
@@ -0,0 +1,24 @@
+var express    = require('express');
+var session    = require('express-session');
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+var flash      = require('connect-flash');
+
+module.exports = {
+  express: expressMiddleware,
+  passport: passportMiddleware
+}
+
+function expressMiddleware (app) {
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(morgan('dev'));
+}
+
+function passportMiddleware(app, passport) {
+	// required for passport
+	app.use(session({ secret: 'secret' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	app.use(flash());
+}
