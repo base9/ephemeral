da2fe9fd4107f897d153ae58d59cae2451988677
Brian Scoles <brianscoles@gmail.com>
2015-01-05 11:05:27 -0800

    set up s3 test route.  note: AWS keys in untracked file.

--- server/api/photos/index.js
+++ server/api/photos/index.js
@@ -1,9 +1,11 @@
 var controller = require('./photos.controller');
 var router = require('express').Router();
 
+
+router.get('/s3', controller.s3);
+
 router.get('/:id', controller.getOne);
 
 router.get('/', controller.getAll);
-
 
 module.exports = router;
--- server/api/photos/photos.controller.js
+++ server/api/photos/photos.controller.js
@@ -1,6 +1,11 @@
 var controller = module.exports;
 var Photo = require('./photos.model.js');
 var seed = require('./photos.seed.js');
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-west-1';
+
+//sets up key and secret as env variables from a file that is .gitignored.
+require('./aws_keys.js');
 
 
 controller.getOne = function(req, res) {
@@ -23,3 +28,14 @@ controller.getAll = function(req, res) {
     res.json(collection);
   });
 };
+
+controller.s3test = function(){
+  console.log('s3 request received')
+  var s3 = new AWS.S3();
+  var params = {Bucket: 'base9photos', Key: 'calvin.jpg'};
+  var file = require('fs').createWriteStream('calvin.jpg');
+  s3.getObject(params).createReadStream().pipe(file)
+  .on('finish',function(thing1,thing2){
+    console.log('download complete.');
+  });
+}
