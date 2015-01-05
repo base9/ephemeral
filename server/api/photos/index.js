2491c47e184a2ab33ae6e5fe220fa31deab453ef
Brian Scoles <brianscoles@gmail.com>
2015-01-05 14:27:05 -0800

    add more test routes for s3 connection

--- server/api/photos/index.js
+++ server/api/photos/index.js
@@ -2,7 +2,11 @@ var controller = require('./photos.controller');
 var router = require('express').Router();
 
 
-router.get('/s3', controller.s3);
+router.get('/s3GetTest', controller.s3GetTest);
+
+router.get('/s3SignedPostTest', controller.s3SignedPostTest);
+
+router.post('/s3PostTest', controller.s3PostTest);
 
 router.get('/:id', controller.getOne);
 
--- server/api/photos/photos.controller.js
+++ server/api/photos/photos.controller.js
@@ -1,14 +1,20 @@
+//helpful: http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-examples.html
+
 var controller = module.exports;
+var request = require('request');
 var Photo = require('./photos.model.js');
 var seed = require('./photos.seed.js');
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-west-1';
+var s3 = new AWS.S3();
+var fs = require('fs');
 
 //sets up key and secret as env variables from a file that is .gitignored.
 require('./aws_keys.js');
 
 
 controller.getOne = function(req, res) {
+//DEPRECATED.  photo get requests should go directly to S3, not through our server.
   Photo.where({id:req.params.id}).fetch({
       withRelated: ['user']
     }).then(function (photo) {
@@ -22,6 +28,7 @@ controller.getOne = function(req, res) {
 };
 
 controller.getAll = function(req, res) {
+//DEPRECATED.  photo get requests should go directly to S3, not through our server.
   Photo.fetchAll({
       withRelated: ['user']
     }).then(function (collection) {
@@ -29,13 +36,60 @@ controller.getAll = function(req, res) {
   });
 };
 
-controller.s3test = function(){
-  console.log('s3 request received')
-  var s3 = new AWS.S3();
-  var params = {Bucket: 'base9photos', Key: 'calvin.jpg'};
-  var file = require('fs').createWriteStream('calvin.jpg');
+
+//POSTING A PHOTO: intended process
+//client sends a GET to api/phots/addnew.  includes user id, event name, file extension, other details.
+//server creates an entry in photos table. generates and returns presigned S3 URL to client.
+//client then uses presigned URL to upload the photo to S3.
+//if successful upload: just say 'photo uploaded!'
+//if error: notify client that upload failed, and notify server so it can drop that recently created record.
+
+//TODO: photo bucket currently allows anonymous read/write access.  refactor this.
+//should allow credentialed read-write access (for dev environment - have api keys cached on dev machine)
+
+//images can be viewed by the client very simply, like this: 
+//<img src='https://s3-us-west-1.amazonaws.com/base9photos/UNIQUE_FILE_NAME.jpg'></img>
+
+controller.s3GetTest = function(){
+  console.log('s3 request received');
+  var fileName = 'calvin.jpg';
+  var params = {Bucket: 'base9photos', Key: fileName};
+  var file = require('fs').createWriteStream(fileName);
   s3.getObject(params).createReadStream().pipe(file)
-  .on('finish',function(thing1,thing2){
-    console.log('download complete.');
+  .on('finish',function(){
+    console.log('download complete of ' + fileName);
   });
 }
+
+controller.s3PostTest = function(){
+  console.log('s3 post request received');
+  var fs = require('fs');
+  var zlib = require('zlib');
+  var fileName = 'ch.jpg';
+
+  var body = fs.createReadStream(fileName).pipe(zlib.createGzip());
+  var s3obj = new AWS.S3({params: {Bucket: 'base9photos', Key: 'ch.zip'}});
+  s3obj.upload({Body: body}).
+    on('httpUploadProgress', function(evt) { console.log(evt); }).
+    send(function(err, data) { console.log(err, data) });
+}
+
+
+controller.s3SignedPostTest = function(req,res){
+  console.log('req received for signed PUT url')
+  var params = {Bucket: 'base9Photos', Key: 'aNewFile.jpg'};
+  var url = s3.getSignedUrl('putObject', params);
+  console.log("The URL is", url);
+  res.json(url);
+}
+
+
+
+
+
+
+
+
+
+
+
