ecf34070836236e022813a9c6d5aec88b2d36152
Brian Scoles <brianscoles@gmail.com>
2015-01-05 16:42:13 -0800

    delete deprecated routes and controller functions

--- server/api/photos/index.js
+++ server/api/photos/index.js
@@ -1,17 +1,6 @@
 var controller = require('./photos.controller');
 var router = require('express').Router();
 
-
-router.get('/s3GetTest', controller.s3GetTest);
-
-router.get('/s3SignedPostTest', controller.s3SignedPostTest);
-
-router.post('/s3PostTest', controller.s3PostTest);
-
 router.post('/addOne', controller.addOne);
-
-router.get('/:id', controller.getOne);
-
-router.get('/', controller.getAll);
 
 module.exports = router;
--- server/api/photos/photos.controller.js
+++ server/api/photos/photos.controller.js
@@ -12,30 +12,6 @@ var fs = require('fs');
 require('./aws_keys.js');
 
 
-controller.getOne = function(req, res) {
-//DEPRECATED.  photo get requests should go directly to S3, not through our server.
-  Photo.where({id:req.params.id}).fetch({
-      withRelated: ['user']
-    }).then(function (photo) {
-      if(photo){
-        res.json(photo);
-      } else {
-        res.status(404).end();
-      }
-
-  });
-};
-
-controller.getAll = function(req, res) {
-//DEPRECATED.  photo get requests should go directly to S3, not through our server.
-  Photo.fetchAll({
-      withRelated: ['user']
-    }).then(function (collection) {
-    res.json(collection);
-  });
-};
-
-
 //POSTING A PHOTO: intended process
 //client sends a GET to api/phots/addnew.  includes user id, event name, file extension, other details.
 //server creates an entry in photos table. generates and returns presigned S3 URL to client.
@@ -49,40 +25,6 @@ controller.getAll = function(req, res) {
 
 //images can be viewed by the client very simply, like this: 
 //<img src='https://s3-us-west-1.amazonaws.com/base9photos/UNIQUE_FILE_NAME.jpg'></img>
-
-controller.s3GetTest = function(){
-  console.log('s3 request received');
-  var fileName = 'calvin.jpg';
-  var params = {Bucket: 'base9photos', Key: fileName};
-  var file = require('fs').createWriteStream(fileName);
-  s3.getObject(params).createReadStream().pipe(file)
-  .on('finish',function(){
-    console.log('download complete of ' + fileName);
-  });
-}
-
-controller.s3PostTest = function(){
-  console.log('s3 post request received');
-  var fs = require('fs');
-  var zlib = require('zlib');
-  var fileName = 'ch.jpg';
-
-  var body = fs.createReadStream(fileName).pipe(zlib.createGzip());
-  var s3obj = new AWS.S3({params: {Bucket: 'base9photos', Key: 'ch.zip'}});
-  s3obj.upload({Body: body}).
-    on('httpUploadProgress', function(evt) { console.log(evt); }).
-    send(function(err, data) { console.log(err, data) });
-}
-
-
-controller.s3SignedPostTest = function(req,res){
-  console.log('req received for signed PUT url')
-  var params = {Bucket: 'base9photos', Key: 'candles.jpg'};
-  var url = s3.getSignedUrl('putObject', params);
-  console.log("The URL is", url);
-  res.json(url);
-  //fs.createReadStream('candles.jpg').pipe(request.put(url));
-}
 
 
 controller.addOne = function(req,res){
