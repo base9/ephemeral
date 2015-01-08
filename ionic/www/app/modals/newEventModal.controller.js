9a8c8652a2a2d161df91f9d7fdc835e43cb33858
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-01-08 12:23:18 -0800

    converts tabs to spaces

--- ionic/www/app/modals/newEventModal.controller.js
+++ ionic/www/app/modals/newEventModal.controller.js
@@ -1,15 +1,15 @@
 angular.module('radar').controller('NewEventModalController', [
-	'MapFactory', 
-	'MarkerFactory', 
-	'HttpHandler', 
-	'$scope', 
-	function(Map, Marker, Http, $scope) {
+  'MapFactory', 
+  'MarkerFactory', 
+  'HttpHandler', 
+  '$scope', 
+  function(Map, Marker, Http, $scope) {
 
-		$scope.saveNewEvent = function() {
-			console.log("SAVING NEW EVENT");
-		};
+    $scope.saveNewEvent = function() {
+      console.log("SAVING NEW EVENT");
+    };
 
-		$scope.test = "HELLO";
+    $scope.test = "HELLO";
 
 
 }]);
