328a40ec5275b7a39526469e5be9c0990fe374b5
Stephan Genyk <sgenyk91@gmail.com>
2015-01-08 16:46:08 -0800

    Creates filters for popularity, category, distance, keywords, time, and cost

---
+++ ionic/www/js/map.controller.js
@@ -0,0 +1,68 @@
+angular.module('radar')
+.controller('MapController', [
+	'MapFactory', 
+	'MapSearchFactory', 
+	'MarkerFactory', 
+	'HttpHandler', 
+  '$ionicModal',
+	'$scope', 
+	function(Map, SearchBox, Marker, Http, $ionicModal, $scope) {
+
+    var listOfMarkers = {};
+  
+    $scope.geoLocate = function(){
+      console.log("LOCATING.......");
+      Map.geoCenter();
+    };
+
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    }
+
+    var map = Map.initialize();
+
+    var myMarker = Map.geoLocate(function(location) {
+      listOfMarkers.location = {'lat': location.latitude, 'lng': location.longitude}
+      console.log("LIST OF MARKERS: ", listOfMarkers);
+    });
+
+
+    Http.getMarkers(function(events) {
+      Marker.placeMarkers(map, events, function(markers) {
+        console.log("EVENTS: ", events);
+        //store marker location and events inside scope for now
+        listOfMarkers.events = events;
+
+        for (var i = 0; i < markers.length; i++) {
+          var title = events[i].title;
+          var marker = markers[i];
+          var event = events[i];
+          //must invoke function in order to grab current marker, title, and rating
+          google.maps.event.addListener(marker, 'click', (function(marker, event) {
+            return function() {
+              $ionicModal.fromTemplateUrl('../templates/eventInfoModal.html', {
+                scope: $scope,
+              }).then(function(modal) {
+                $scope.modal = modal;
+                $scope.openModal();
+              });
+              $scope.title = event.title;
+              $scope.info = event.info;
+              $scope.startTime = event.startTime;
+              $scope.endTime = event.endTime;
+            }
+          })(marker, event));
+        }
+      });
+    });
+
+    this.filter = function(filters) {
+      Marker.filterMarkers(map, listOfMarkers, filters);
+    }
+
+    window.uglyScope = $scope;
+
+}]);
---
+++ ionic/www/js/marker.factory.js
@@ -0,0 +1,245 @@
+angular.module('radar')
+.factory('MarkerFactory', function() {
+  var markerObj = {};
+  markers = [];
+
+  //filters is an object with properties popularity, category, distance, and keyword
+  markerObj.placeMarkers = function(map, events, callback) {
+
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+
+    if (events.length === 0) {
+      return;
+    }
+
+    // create new markers for all events
+    createMarkers(map, events, markers, bounds);
+
+    map.fitBounds(bounds);
+
+    if (events.length) {
+      map.setZoom(14);
+    }
+
+    // var ratings = event.ratings;
+    callback(markers);
+  }
+  //not using other above function because no callback is needed
+  markerObj.filterMarkers = function(map, objFilters, filters) {
+
+    // remove all existing markers
+    clearMarkers(markers);
+    var markers = [];
+    var bounds = new google.maps.LatLngBounds();
+
+    var events = objFilters.events;
+    if (filters) {
+      //filter.popularity is a number
+      if (filters.popularity) {
+        events = filterPopularity(events, filters.popularity);
+      }
+      //filter.category is a string
+      if (filters.category) {
+        events = filterCategory(events, filters.category);
+      }
+      //filter.distance is number in miles
+      if (filters.distance) {
+        events = filterDistance(events, objFilters.location, filters.distance);
+      }
+      //filters.keyword is an array of stringed keywords
+      if (filters.keywords) {
+        var temp = filterKeyword(events, filters.keywords);
+        if (temp.foundMatch) {
+          events = temp.results;
+        } else {
+          events = [];
+          //return that the keywords do not match any event
+        }
+      }
+      //filters.cost is an object with lowEnd and highEnd properties containing a number
+      if (filters.cost) {
+        events = filterCost(events, filters.cost.lowEnd, filters.cost.highEnd);
+      }
+      //filters.time is an object with new, startTime, and endTime as a boolean, string, and string, respectively
+      //The string must be capable of being parsed by Date.parse()
+      if (filters.time.now || filters.time.startTime || filters.time.endTime) {
+        events = filterTime(events, filters.time.now, filters.time.startTime, filters.time.endTime);
+      }
+
+    }
+
+    createMarkers(map, events, markers, bounds);
+
+    map.fitBounds(bounds);
+
+    if (events.length) {
+      map.setZoom(14);
+    }
+  }
+
+/********************* HELPER FUNCTIONS *********************/
+  
+  var createMarkers = function(map, events, markers, bounds) {
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      var position = new google.maps.LatLng(event.lat, event.lng);
+
+      markers.push(new google.maps.Circle({
+        map: map,
+        title: event.title,
+        // icon: image,
+        position: position,
+        strokeColor: 'green',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: 'green',
+        fillOpacity: 0.35,
+        center: position,
+        radius: Math.random()*150
+      }));
+
+      bounds.extend(position);
+    }
+  }
+
+  //Removes all markers on the map
+  var clearMarkers = function(markers) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+  }
+
+  var filterPopularity = function(events, popularity) {
+    var results = [];
+    for (var i = 0; i < events.length; i++) {
+      if (events[i].popularity >= popularity) {
+        results.push(events[i]);
+      }
+    }
+    return results;
+  }
+
+  var filterCategory = function(events, category) {
+    var results = [];
+    for (var i = 0; i < events.length; i++) {
+      if (events[i].category === category) {
+        results.push(events[i]);
+      }
+    }
+    return results;
+  }
+
+  var filterDistance = function(events, userLocation, distance) {
+    var results = [];
+    var parameters = latitudeLongitudeToDistanceConverter(userLocation.lat, userLocation.lng, distance);
+    for (var i = 0; i < events.length; i++) {
+      if (withinCoordinates(events[i], userLocation, parameters)) {
+        results.push(events[i]);
+      }
+    }
+    return results;
+  }
+
+  var filterKeyword = function(events, keywords) {
+    var results = [];
+    for (var j = 0; j < keywords.length; j++) {
+      var match = false;
+      for (var i = 0; i < events.length; i++) {
+        if (keywords[j] === events[i]) {
+          match = true;
+          break;
+        }
+      }
+      if (!match) {
+        return {'foundMatch': false};
+      }
+    }
+    return {'foundMatch': true, 'results': results};
+  }
+
+  var filterCost = function(events, lowCost, highCost) {
+    var results = [];
+    for (var i = 0; i < events.length; i++) {
+      if (events[i].cost >= lowCost && events[i].cost <= highCost) {
+        results.push(events[i]);
+      }
+    }
+    return results;
+  }
+
+  //All time will be dealt in number of milliseconds since Jan 1, 1970
+  var filterTime = function(events, now, startTime, endTime) {
+    var results = [];
+    var twoHoursInMilliseconds = 7200000;
+    var start;
+    var end;
+    for (var i = 0; i < events.length; i++) {
+      if (now) {
+        start = Date.now();
+        end = Date.now() + twoHoursInMilliseconds;
+      } else {
+        start = Date.parse(startTime);
+        if (endTime) {
+          end = Date.parse(endTime);
+        } else {
+          //If no end time is specified, then end time will be set to 3 a.m. of the next day
+          var temp = new Date();
+          temp.setDate(temp.getDate() + 1);
+          end = temp.setHours(3, 0, 0, 0);
+        }
+      }
+      if (events[i].endTime >= start && events[i].startTime <= end) {
+        results.push(events[i]);
+      }
+    }
+    return results;
+  }
+
+  var latitudeLongitudeToDistanceConverter = function(lat, lng, d) {
+
+    Number.prototype.toRadians = function() {
+      return this * Math.PI / 180;
+    }
+    Number.prototype.toDegrees = function() {
+      return this * 180 / Math.PI;
+    }
+
+    //Radius of Earth in miles
+    var R = 3959;
+
+    //Converts degrees to radians
+    var lat1 = lat.toRadians();
+    var lng1 = lng.toRadians();
+
+    //find second latitude from 0º clockwise from North
+    var lat2North = Math.asin(Math.sin(lat1) * Math.cos(d/R) + Math.cos(lat1) * Math.sin(d/R) * Math.cos(0));
+
+    //find second longitude from 90º clockwise from North (needs lat2East to find second longitude)
+    var lat2East = Math.asin(Math.sin(lat1) * Math.cos(d/R) + Math.cos(lat1) * Math.sin(d/R) * Math.cos(Math.PI / 2));
+    var lng2East = lng1 + Math.atan2(Math.sin(Math.PI / 2) * Math.sin(d/R) * Math.cos(lat1), Math.cos(d/R) - Math.sin(lat1) * Math.sin(lat2East));
+
+    // var Δx = x2 - x1;
+    // var Δy = y2 - y1;
+
+    // //Haversine formula to find greatest circle distance between two points
+    // var a = Math.sin(Δx/2) * Math.sin(Δx/2) + Math.cos(x1) * Math.cos(x2) * Math.sin(Δy/2) * Math.sin(Δy/2);
+    // var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    // var d = R * c;
+
+    return {'north': lat2North.toDegrees() - lat, 'east': lng2East.toDegrees() - lng};
+  }
+
+  var withinCoordinates = function(events, userLocation, parameters) {
+    if (events.lat <= userLocation.lat + parameters.north 
+     && events.lat >= userLocation.lat - parameters.north
+     && events.lng <= userLocation.lng + parameters.east
+     && events.lng >= userLocation.lng - parameters.east) {
+      return true;
+    }
+    return false;
+  }
+
+  return markerObj;
+});
+
