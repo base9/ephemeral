2f22c7e40b7a26b028bcb985a2daa54a662227d1
Stephan Genyk <sgenyk91@gmail.com>
2015-01-10 21:41:19 -0800

    Rondom fixes

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,9 @@ var knex = require('knex');
 var path = require('path');
 var pg = require('pg');
 
-    var db = knex({
-      client: 'pg',
-      connection: process.env.DATABASE_URL || 'postgres://localhost:5432/base9'
-    });
+  var db = knex({
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost:5432/base9'
+  });
 
 module.exports = db;
