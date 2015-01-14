dca581eb645babbe531eb52d7320af921dc28e20
Stephan Genyk <sgenyk91@gmail.com>
2015-01-13 20:41:09 -0800

    Almost finishes on fixing info windows from disappearing

--- ionic/www/app/map/map.controller.js
+++ ionic/www/app/map/map.controller.js
@@ -8,7 +8,8 @@ angular.module('radar')
   '$scope', 
   function(Map, SearchBox, Marker, Http, $ionicModal, $scope) {
 
-    var listOfMarkers = {};
+    var listOfEvents = {};
+    var matches;
   
     $scope.geoLocate = function(){
       console.log("LOCATING.......");
@@ -38,34 +39,41 @@ angular.module('radar')
     }
 
     Map.geoLocate(function(location, bounds) {
-      listOfMarkers.location = {'lat': location.latitude, 'lng': location.longitude};
-      listOfMarkers.bounds = {ne: bounds.getNorthEast(), sw: bounds.getSouthWest()};
-      console.log("BOUNDS: ", listOfMarkers.bounds);
-      console.log("LIST OF MARKERS: ", listOfMarkers);
+      listOfEvents.location = {'lat': location.latitude, 'lng': location.longitude};
+      listOfEvents.bounds = {ne: bounds.getNorthEast(), sw: bounds.getSouthWest()};
+      console.log("BOUNDS: ", listOfEvents.bounds);
+      console.log("LIST OF MARKERS: ", listOfEvents);
 
-      Http.getMarkers(listOfMarkers.bounds, function(events) {
+      Http.getMarkers(listOfEvents.bounds, function(events) {
         Marker.placeMarkers(map, events, function(markers) {
           console.log("EVENTS: ", events);
           //store marker location and events inside scope for now
-          listOfMarkers.events = events;
-
+          listOfEvents.events = events;
+          matches = {};
           for (var i = 0; i < markers.length; i++) {
             var title = events[i].title;
             var marker = markers[i];
             var event = events[i];
+            matches[events[i]] = markers[i];
             //must invoke function in order to grab current marker, title, and rating
             google.maps.event.addListener(marker, 'click', (function(marker, event) {
               return function() {
+                Http.getOneEvent(event.id, function(res) {
+                  $scope.eventInfo = res;
+                  ($scope.eventInfo.price === "0.00") ? $scope.eventInfo.price = "Free" : $scope.eventInfo.price = '$'+ $scope.eventInfo.price;
+                  $scope.eventInfo.startDate = isoDateToTimeString(parseInt($scope.eventInfo.startTime));
+                  $scope.eventInfo.endDate = isoDateToTimeString(parseInt($scope.eventInfo.endTime));
+                  $scope.eventInfo.mainPhoto = $scope.eventInfo.photos[0];
+                  $scope.eventInfo.photos = $scope.eventInfo.photos.slice(1);
+                  $scope.eventInfo.comments = $scope.eventInfo.comments.reverse();
+                });
                 $ionicModal.fromTemplateUrl('./app/modals/eventInfoModal.html', {
                   scope: $scope,
-                }).then(function(modal) {
+                })
+                .then(function(modal) {
                   $scope.modal = modal;
                   $scope.openModal();
                 });
-                $scope.title = event.title;
-                $scope.info = event.info;
-                $scope.startTime = event.startTime;
-                $scope.endTime = event.endTime;
               };
             })(marker, event));
           }
@@ -87,7 +95,8 @@ angular.module('radar')
     if (this.filters.popularity === 0) {
       this.filters.popularity = null;
     }
-    Marker.filterMarkers(map, listOfMarkers, this.filters);
+    Marker.filterMarkers(map, listOfEvents, this.filters, matches);
+
     this.filters = {distance: 0, popularity: 0, time: {now: false, startTime: null, endTime: null}, cost: 0, keyword: null};
   };
 
