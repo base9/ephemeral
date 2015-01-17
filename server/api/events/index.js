d1293c9e582e36b89874667e693972bef39ecee1
espiler <monkeysrawesome@gmail.com>
2015-01-16 19:42:01 -0800

    removes auth dev mode

--- server/api/events/index.js
+++ server/api/events/index.js
@@ -24,6 +24,6 @@ router.get('/', controller.getAll);
 // POST request to /api/events will go to controller.addOne only if logged in,
 // otherwise it would redirect to homepage
 
-router.post('/', /* auth.isLoggedIn ,*/ controller.addOne);
+router.post('/', auth.isLoggedIn, controller.addOne);
 
 module.exports = router;
