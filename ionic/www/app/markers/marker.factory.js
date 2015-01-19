aeb77428c6a48ba483dcd3b0a346f59ca2fa0889
espiler <monkeysrawesome@gmail.com>
2015-01-18 20:51:51 -0800

    completes popularity glow

--- ionic/www/app/markers/marker.factory.js
+++ ionic/www/app/markers/marker.factory.js
@@ -107,7 +107,7 @@ angular.module('radar')
         + '">'
         + '<img src="' + ringUrl + '" class="ring circle-animation '+ happeningNow + ' " /></div>'
         + '<img src="' + ringUrl + '" class="ring circle-animation circle-animation-delay '+ happeningNow + '2 " /></div>'
-        + '<img src="img/markers/marker.svg" class="marker shadow ' + popularity + '' + happeningNow + 'Marker" />'
+        + '<img src="img/markers/marker.svg" class="marker shadow ' + popularity + '' + eventCategory + '" />'
         + '<img src="' + markerUrl + '" class="marker ' + popularity + '' + happeningNow + 'Marker" />'
         // + '<filter id="drop-shadow"><feMorphology operator="dilate" radius="6" in="SourceAlpha" result="dilated"/><feGaussianBlur stdDeviation="3" in="dilated" result="blurred"/><feFlood flood-color="rgba(255,255,255,0.5)" in="blurred" result="flooded"/><feMerge><feMergeNode/><feMergeNode in="'+ markerUrl +'"/></feMerge></filter>'
         + '</img></div>';
